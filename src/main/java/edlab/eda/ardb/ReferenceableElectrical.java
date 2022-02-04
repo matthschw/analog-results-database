7fcbfdd684e9b66958c2522ec7a5af76e40c1d19
Matthias Schweikardt <Matthias.Schweikardt@reutlingen-university.de>
2022-02-04 13:44:59 +0100

    new electrical properties

--- src/main/java/edlab/eda/ardb/ReferenceableElectrical.java
+++ src/main/java/edlab/eda/ardb/ReferenceableElectrical.java
@@ -2,10 +2,13 @@ package edlab.eda.ardb;
 
 /**
  * A class which implements this interface represents an object that is able to
- * provide an identifier that references an electrical magnitude (voltage,
- * current, operating point) in a circuit.
+ * provide an identifier that references a net, terminal or operating point
  */
 public interface ReferenceableElectrical {
+
+  public enum TYPE {
+    NET, TERMINAL, OPPOINT
+  }
 
   /**
    * Get an identifier as string
@@ -13,4 +16,19 @@ public interface ReferenceableElectrical {
    * @return identifier
    */
   public String getIdentifier();
+
+  /**
+   * Get hierarchical address to the corresponding net, terminal or operating
+   * point
+   * 
+   * @return address
+   */
+  public String[] getAddress();
+
+  /**
+   * Get the type of the {@link ReferenceableElectrical} as enum {@link TYPE}
+   * 
+   * @return type of the electrical property
+   */
+  public TYPE getType();
 }
